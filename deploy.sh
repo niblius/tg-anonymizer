fa249e91b2e7de28160f7452f43ddff5c99c2855
niblius <niblius333@gmail.com>
2019-06-04 02:50:27 -0400

    Deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ IMG="anonymizer-img"
 
 docker rm -f $(docker stop $(docker ps -a -q --filter ancestor="$IMG" --format="{{.ID}}")) && \
   docker build -t "$IMG" . && \
-  docker run -d --env ANONYMIZER_BOT_TOKEN=$ANONYMIZER_BOT_TOKEN --env BOT_ENVIRONMENT="PRODUCTION" --name="anonymizer-$INSTANCE" anonymizer-img
+  docker run -d --net=host --env ANONYMIZER_BOT_TOKEN=$ANONYMIZER_BOT_TOKEN --env BOT_ENVIRONMENT="PRODUCTION" --name="anonymizer-$INSTANCE" anonymizer-img
 
